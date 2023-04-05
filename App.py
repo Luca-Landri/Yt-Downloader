f4ed3b02dc3f556beff279e103fd52d0381d3321
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-04-05 13:49:31 +0200

    Feat: Added other style to the app

--- App.py
+++ App.py
@@ -1,14 +1,22 @@
 import tkinter as tk
+from pytube import YouTube
+
+
 root = tk.Tk()
 root.geometry("600x400")
 root.title("Landri Downloader")
 
+frame = tk.Frame(root)
+frame.place(relx=0.5, rely=0.5, anchor="center")
 
-label = tk.Label(root, text="Hello World", font=("Helvetica", 24))
-label.pack(side=tk.TOP, pady = (150, 0))
+
+label = tk.Label(frame, text="INSERT THE LINK", font=("Helvetica", 35), fg="red", width=20, height=2)
+input_field = tk.Entry(frame, font=("Helvetica", 24))
+button = tk.Button(frame, text="Download", font=("Helvetica", 24), command=lambda: download(input_field.get()), pady=10, padx=10)
 
 
-button = tk.Button(root, text="Click Me")
-button.pack(side=tk.BOTTOM, pady = (0, 150))
+label.pack(side="top", fill="both")
+input_field.pack( side="top", fill="both")
+button.pack(side=tk.TOP)
 
 root.mainloop()
